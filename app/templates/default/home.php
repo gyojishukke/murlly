e8d8fc2f4e557084ca7b9448010cf035f0268dd2
gyojishukke <thierry@mezenge.fr>
2016-05-11 11:23:09 +0200

    sauvegarde

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -3,6 +3,11 @@
 
 
 <?php $this->start('main_content') ?>
+<nav>
+  <li>a</li>
+    <li>a</li>
+      <li>a</li>
+</nav>
 
 <!-- <p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p> -->
 <!-- include -->
